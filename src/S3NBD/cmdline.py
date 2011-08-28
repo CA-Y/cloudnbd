ed563f2a4e660abbd4f29781a43eca376c3c8e3e
Mansour <mansour@oxplot.com>
2011-08-28 18:32:46 +1000

    Some work on command line parsing

---
+++ src/S3NBD/cmdline.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+#
+# cmdline.py - Command line utils
+# Copyright (C) 2011  Mansour <mansour@oxplot.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+import sys
+import re
+import argparse
+import S3NBD
+
+def _storage_size(value):
+  """Parse a size with possible letter multipliers and return the actual
+  value as integer.
+  """
+  m = re.match(ur'(\d+)([kmgtp]?)', value.strip().lower())
+  if m:
+    im = {'': 1e0, 'k': 1e3, 'm': 1e6,
+          'g': 1e9, 't': 1e12, 'p': 1e15}
+    return int(int(m.group(1)) * im[m.group(2)])
+  else:
+    msg = "%s must be in the form: <int><K|M|G|T|P>"
+    raise argparse.ArgumentTypeError(msg)
+
+def add_name_args(parser):
+  """Add volume bucket and name arguments to the parser."""
+  parser.add_argument(
+    'bucket',
+    type=str,
+    help="S3 bucket name where volume to be stored"
+  )
+  parser.add_argument(
+    'volume',
+    type=str,
+    help="name of the volume"
+  )
+
+def add_size_arg(parser, as_arg = False):
+  """Add size argument to the parser.
+
+  Parameters:
+    as_arg - if True, makes size a required positional argument instead
+             of an optional one
+  """
+  parser.add_argument(
+    *(['size'] if as_arg else ['-s', '--size']),
+    type=_storage_size,
+    help="default size of volume as reported to NBD client -"
+         " e.g. 100T which is 100 terabytes"
+  )
+
+def add_blocksize_arg(parser):
+  """Add block size argument to the parser."""
+  parser.add_argument(
+    '-b', '--block-size',
+    type=_storage_size,
+    help="block size of block as stored on S3 -"
+         " e.g. 100T which is 100 terabytes"
+  )
+
+def add_server_args(parser):
+  """Add NBD server related arguments to parser."""
+  parser.add_argument(
+    '-i', '--bind-address',
+    type=str,
+    help="the IP address the NBD server will be bound to"
+  )
+  parser.add_argument(
+    '-p', '--port',
+    type=int,
+    help="the port the NBD server will listen on"
+  )
+
+def add_auth_args(parser):
+  """Add authentication related arguments to the parser."""
+  parser.add_argument(
+    '-a', '--access-key',
+    type=str,
+    help="S3 access key"
+  )
+  parser.add_argument(
+    '-k', '--secret-key',
+    type=str,
+    help="S3 secret key"
+  )
+  parser.add_argument(
+    '-y', '--passphrase',
+    type=str,
+    help="passphrase used to enrypt data on S3"
+  )
+
+def add_common_args(parser):
+  """Add common arguments to the parser."""
+  parser.add_argument(
+    '-v', '--version',
+    action='store_true',
+    help="show the program version and exit"
+  )
--- src/s3bd_init.py
+++ src/s3bd_init.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# s3bd.py - Main executable of s3bd
+# s3bd_init.py - Initialize a new S3 volume
 # Copyright (C) 2011  Mansour <mansour@oxplot.com>
 #
 # This program is free software: you can redistribute it and/or modify
@@ -23,45 +23,26 @@ from __future__ import division
 import sys
 import argparse
 import S3NBD
+#from S3NBD import auth
+from S3NBD import cmdline
 
 def main():
-  commands = ['init', 'open', 'close', 'resize', 'closeall',
-              'info', 'stat', 'list']
-  if len(sys.argv) > 1 and sys.argv[1] in commands:
-    cmd = sys.argv[1]
-    del sys.argv[1]
-    sys.argv[0] += ' ' + cmd
-    exec('import s3bd_%s' % cmd)
-    exec('s3bd_%s.main()' % cmd)
-    exit(0)
-  
   parser = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter,
-    description="NBD server with Amazon S3 as the backend",
-    epilog="""Following is a short description of each command:
-  close:    close an open volume
-  closeall: close all open volumes
-  info:     show volume info of an S3 volume
-  init:     initialize a new S3 volume
-  list:     list the currently open volumes
-  open:     open an existing volume
-  resize:   set the default size for an S3 volume
-  stat:     show the statistics of an open S3 volume
+    description="Initialize a new S3 volume",
+  )
+  cmdline.add_common_args(parser)
+  cmdline.add_name_args(parser)
+  cmdline.add_size_arg(parser, as_arg=True)
+  cmdline.add_auth_args(parser)
 
-For more info about each command, use '-h'.
-"""
-  )
-  parser.add_argument(
-    'command',
-    type=str,
-    choices=commands,
-    help="command to run - for more info about each, add -h"
-  )
-  parser.add_argument('-v', '--version', action='store_true')
   args = parser.parse_args()
 
   if args.version:
     print(S3NBD.__version__)
+    exit(0)
+
+  print(repr(args))
 
 if __name__ == '__main__':
   main()
